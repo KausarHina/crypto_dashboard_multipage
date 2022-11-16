55dbd74c41064e631ad3cc0879822d292fbd8557
marissagonzales468 <marissagonzales468@gmail.com>
2022-11-15 19:52:20 -0800

    adding pycoingecko

---
+++ app/pycoingecko/Coin_Price_Chart_Widget.py
@@ -0,0 +1,53 @@
+Bitcoin:
+html = <script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget  coin-id="bitcoin" currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
+
+Ethereum: 
+html = <script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget  coin-id="ethereum" currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
+
+EOS:
+html = <script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget  coin-id="eos" currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
+
+Litecoin:
+html = <script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget  coin-id="litecoin" currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
+
+Ripple:
+html = <script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget  coin-id="ripple" currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
+
+
+Cardano:
+html = <script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget  coin-id="cardano" currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
+
+
+Polkadot:
+html = <script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget  coin-id="polkadot" currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
+
+
+Tether:
+html = <script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget  coin-id="tether" currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
+
+
+
+BinanceCoin:
+html = <script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget  coin-id="binancecoin" currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
+
+
+USD Coin: 
+html = <script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget  coin-id="usd-coin" currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
+
+Dogecoin:
+html = <script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget  coin-id="dogecoin" currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
+
+Polygon:
+html = <script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget  coin-id="matic-network" currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
